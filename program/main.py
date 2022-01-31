faeb6f5f9c17ceed96a6f17044e4c16499d92299
Jakub Kolář <78088608+Jakewh@users.noreply.github.com>
2022-01-31 15:18:40 +0100

    funkční script

--- program/main.py
+++ program/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import bar_chart_race as bcr
+import os
 
 """
  ____ _____ ____              _                                  _     
@@ -11,24 +12,24 @@ import bar_chart_race as bcr
 """
 
 # vstupní data
-df = pd.read_csv("/home/jakub/GitHub/Bitcoin_price_graph/data/BTCUSD_day.csv", index_col = "Date"),
+os.chdir("/home/jakub/GitHub/Bitcoin_price_graph/data")  # složka s csv souborem
+data = pd.read_csv("BTCUSD_day.csv")  # název csv souboru
 
+# příprava dat
+cols = ["Date", "BTCUSD"]  # vybrané sloupce tabulky
+Subsetdf = data[cols]  # zobrazení pouze vybraných sloupců v grafu
+Subsetdf.set_index("Date", inplace = True)
+
+# nastavení grafu
 bcr.bar_chart_race(
-    df = df[:1],
-    period_fmt = "% Y, % b % -d ",
-    # výstupní soubor
-    filename = "/home/jakub/Github/Bitcoin_price_graph/video.mp4",
-    # složka s logem BTC
-    img_label_folder = "/home/jakub/Github/Bitcoin_price_graph/data/image/",
-    # nastavení loga BTC
-    fig_kwargs = {  
+    df = Subsetdf,  # data pro graf
+    filename = "/home/jakub/GitHub/Bitcoin_price_graph/BTCUSD.mp4",  # výstupní soubor
+    img_label_folder = "/home/jakub/GitHub/Bitcoin_price_graph/data",  # složka s obrázkem btc
+    # nastavení obázku btc
+    fig_kwargs = {
         "figsize": (26, 15),  # velikost
         "dpi" : 120,  #  dpi
         "facecolor" : "#F8FAFF"  # barva pozadí
     },
-    steps_per_period = 50,
-    period_length = 1500,
-    n_bars = 1647,
-    fixed_max = False
+    title = "Vývoj ceny BTCUSD"  # titulek grafu
 )
-
